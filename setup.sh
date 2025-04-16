0e3a87ff5851b7d9278aafa7a3b4daa428d7fcb6
oezeb <eouedraogo4@gmail.com>
2025-04-16 09:14:47 +0800

    using systemd

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get install -y shadowsocks-libev simple-obfs iptables
-
-chmod +x ./ssmgr.sh
-chmod +x ./ssmgr.conf
-chmod +x ./data_usage_limit.sh
-chmod +x ./data_usage_reset.sh
